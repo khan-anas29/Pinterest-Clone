62fcd0d31b1efc9260ee4775ec290f52d2371275
Anas <anascollege2129@eng.rizvi.edu.in>
2024-02-13 22:57:07 +0530

    Register Function Done

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const userModel = require('./users');
+const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,7 +14,22 @@ router.get('/register', function(req, res, next) {
 });
 // Form Data
 router.post('/register', function(req, res, next) {
-  res.render('register');
+  const data= new userModel({
+    // leftside: user.js
+    // right side: register.ejs
+    username: req.body.username,
+    email: req.body.email,
+    contact: req.body.contact
+  });
+
+  // registering by authenticate
+  userModel.register(data,req.body.password)
+  .then(function(){
+    passport.authenticate("local")(req,res,function(){
+      res.redirect("/profile")
+    })
+  })
+
 });
 
 module.exports = router;
